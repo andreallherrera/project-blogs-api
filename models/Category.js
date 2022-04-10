8c074ae959fa5d0835c26ccfc455bd601f5013c7
Andrea <andrealh08@gmail.com>
2022-04-10 12:15:21 -0300

    feat: create Categories model and migration

---
+++ models/Category.js
@@ -0,0 +1,12 @@
+const Category = (sequelize, DataTypes) => {
+  const category = sequelize.define('Category', {
+    id: DataTypes.NUMBER,
+    name: DataTypes.STRING,
+  }, {
+    timestamps: false,
+  });
+
+  return category;
+};
+
+module.exports = Category;
