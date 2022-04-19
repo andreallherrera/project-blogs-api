15b0513f5ba37cfefe4751a885d53be834d2741b
Andrea <andrealh08@gmail.com>
2022-04-19 20:44:57 -0300

    feat: add login route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
+
 const user = require('./user');
+const login = require('./login');
 
 const router = Router();
 
 router.use('/user', user);
+router.use('/login', login);
 
 module.exports = router;
