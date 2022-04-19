590995f48903f45e978f83473602239f7a253e08
Andrea <andrealh08@gmail.com>
2022-04-19 13:16:35 -0300

    test: function readByEmail tests

--- myTests/mocks/userMocks.js
+++ myTests/mocks/userMocks.js
@@ -1,4 +1,11 @@
 module.exports = {
+  params: {
+    displayName: 'Brett Wiltshire',
+    email: 'brett@email.com',
+    password: '123456',
+    image: 'http://4.bp.blogspot.com/_YA50adQ-7vQ'
+    + '/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png',
+  },
   inserted: {
     id: 1,
     displayName: 'Brett Wiltshire',
@@ -7,7 +14,7 @@ module.exports = {
     image: 'http://4.bp.blogspot.com/_YA50adQ-7vQ'
     + '/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png',
   },
-  response: {
+  create: {
     status: 201, 
     content: {
       token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQsImRpc3BsYXlOYW1lIjoiQnJldHQgV2lsdHNo'
@@ -16,5 +23,22 @@ module.exports = {
         + 'dSV1pEZy9TNDUvYnJldHQucG5nIiwiaWF0IjoxNjQ5NjQ3OTMwLCJleHAiOjE2NTAyNTI3MzB9.DqAb15M'
         + 'p29D977ec4JFC3wL9uttjWPeqIELoXFNDZgg',
     },
+  },
+  readOk: {
+    status: 200, 
+    content: {
+      dataValues: {
+        id: 1,
+        displayName: 'Rubinho Barrichello',
+        email: 'rubinho@gmail.com',
+        password: '123456',
+        image: 'https://www.globalframe.com.br/gf_base/empresas/MIGA/imagens/' 
+        + 'BDA23B2786FD3B7EC65745DC3FA1EE49D31B_barrichello-1.jpg',
+      },
+    },
+  },
+  readNotOk: {
+    status: 200, 
+    content: null,
   },
 };
--- myTests/services/userService.test.js
+++ myTests/services/userService.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 const sinon = require('sinon');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
@@ -12,16 +13,48 @@ const userMock = require('../mocks/userMocks');
 describe('User service', () => {
   describe('#create', () => {
     before(async () => {
-      sinon.stub(User, 'create').resolves(userMock.response);
+      sinon.stub(User, 'create').resolves(userMock.create);
     });
 
     after(() => User.create.restore());
 
-    it('It must return an object with status and content', async () => {
-      const { displayName, email, password, image } = userMock.inserted;
-      const user = await User.create({ displayName, email, password, image });
-      expect(user.status).to.eq(userMock.response.status);
-      expect(user).to.have.any.keys('content');
+    it('It must return a token as a content with status 201', async () => {
+      const user = await User.create(userMock.params);
+      expect(user.status).to.eq(201);
+      expect(user.content).to.have.any.keys('token');
+    });
+  });
+
+  describe('#readByEmail', () => {
+    describe('When an user is found', () => {
+      before(async () => {
+        sinon.stub(User, 'readByEmail').resolves(userMock.readOk);
+      });
+  
+      after(() => User.readByEmail.restore());
+  
+      it('It must return the user with status 200', async () => {
+        const { email } = userMock.inserted;
+        const user = await User.readByEmail(email);
+        expect(user.status).to.eq(200);
+        expect(user.content).to.have.any.keys('dataValues');
+      });
+    });
+
+    describe('When an user is not found', () => {
+      before(async () => {
+        sinon.stub(User, 'readByEmail').resolves(userMock.readNotOk);
+      });
+  
+      after(() => User.readByEmail.restore());
+  
+      it('It must return the user', async () => {
+        const { email } = userMock.inserted;
+        const user = await User.readByEmail(email);
+        expect(user.status).to.eq(200);
+        // eslint-disable-next-line no-unused-expressions
+        expect(user.content).to.be.null;
+      });
     });
   });
 });
