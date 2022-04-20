0d9b4878bf3c1e4098ed3f584accd6b087990c0b
Andrea <andrealh08@gmail.com>
2022-04-20 16:56:21 -0300

    feat: create token middleware and route GET /user

---
+++ middlewares/tokenMiddleware.js
@@ -0,0 +1,14 @@
+const jwt = require('../utils/jwt');
+const { status, messages } = require('../utils/errors');
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token === undefined || token === '') {
+    return res.status(status.notFound).json({ message: messages.TOKEN_NOT_FOUND });
+  }
+  const verified = jwt.validateJWT(token);
+  if (!verified) return res.status(status.notFound).json({ message: messages.INVALID_TOKEN });
+  next();
+};
+
+module.exports = { verifyToken };
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 const userMiddleware = require('../middlewares/userMiddleware');
+const tokenMiddleware = require('../middlewares/tokenMiddleware');
 
 const app = express();
 
 app.post('/', userMiddleware.validateParams, userController.create);
+app.get('/', tokenMiddleware.verifyToken);
 
 module.exports = app;
