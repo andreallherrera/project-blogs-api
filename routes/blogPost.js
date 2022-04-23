4c031fe52adf3c3d04a500673ab56a1cb6d791c8
Andrea <andrealh08@gmail.com>
2022-04-23 01:18:20 -0300

    feat: add delete endpoint

--- routes/blogPost.js
+++ routes/blogPost.js
@@ -9,7 +9,8 @@ app.post('/', tokenMiddleware.verifyToken,
   blogPostMiddleware.validateParams, blogPostController.create);
 app.get('/', tokenMiddleware.verifyToken, blogPostController.read);
 app.get('/:id', tokenMiddleware.verifyToken, blogPostController.readOne);
-app.put('/:id', tokenMiddleware.verifyToken, 
+app.put('/:id', tokenMiddleware.verifyToken, blogPostMiddleware.verifyUser,
   blogPostMiddleware.validateUpdateParams, blogPostController.update);
-
+app.delete('/:id', tokenMiddleware.verifyToken, blogPostMiddleware.verifyUser,
+  blogPostController.deletePost);
 module.exports = app;
